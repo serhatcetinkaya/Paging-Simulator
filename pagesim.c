7afaf9a0c2534018a24673edbf100977365a6163
serhat çetinkaya <serhatcetinkayaa@gmail.com>
2016-12-15 13:57:45 +0200

    v2.1
    -r case eksik

--- pagesim.c
+++ pagesim.c
@@ -1,185 +1,164 @@
+/*
+Copyright (C) 2016  Serhat Çetinkaya serhatcetinkayaa@gmail.com
+                    Javid Karimbayli karimbayli.j@gmail.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <pthread.h>
 #include <unistd.h>
 
-char *hexToBin(char *buffer);
-char *pageLevelOne(char *binary);
-char *pageLevelTwo(char *binary);
-char *pageOffset(char *binary);
-int binToDecimal(char *binary);
+struct list{
+  unsigned int start;
+  unsigned int end;
+  struct list *next;
+}; typedef struct list LIST;
 
-struct physical_address {
-  int *virt_addr;
-};
-typedef struct physical_address PHY_A;
+struct frame{
+  unsigned int virtual_address;
+  int entry_time, used_time;
+}; /*typedef struct frame FRAME;*/
+
+struct frame *frames;
 
 int main(int argc, char *argv[]){
+  FILE *input1 = fopen(argv[1], "r");
+  FILE *input2 = fopen(argv[2], "r");
+  FILE *out = fopen(argv[4], "a");
+  unsigned int x , a, start, end, target;
+  unsigned int pg1, pg2, offset;
+  unsigned int offset_perde = 0xfff;
+  unsigned int page2perde = 0x3ff;
+  unsigned int check, victim;
+  char buffer[10];
+  int flag, empty = 0, i, j, entry_time = 1, used_time = 1;
+  int M = atoi(argv[3]);
+  int frame_size = M*4096;
+  int algo = atoi(argv[6]);
+  LIST *head = NULL, *current;
 
-  char *arg = (char *) argv[1];
-  if (arg[0] != 45){
-    ///// first parameter is not -r
-    int M = (int) atoi(argv[3]);
-    int frame_array_size = M * 4096;
-    int algo = (int) atoi(argv[6]);
-    int i, j, k;
-    char *binary = (char *) malloc(sizeof(char)*32);
-    char *pt1 = (char *) malloc(sizeof(char)*10);
-    char *pt2 = (char *) malloc(sizeof(char)*10);
-    char *offset = (char *) malloc(sizeof(char)*12);
-    FILE *fp1 = fopen(argv[1], "r");
-    FILE *fp2 = fopen(argv[2], "r");
-    //--------------------------------------------------------------------
-    // page table level 1 array will be **int array of size 1024
-    // page table level 2 array will be *int array of size 1024
-    // page table offset array will be int array of size 4096
-    // physical address table will be PHY_A array of size M*4096
-    // we will parse first 10 digits of binary form of the hex input and
-    // reach the index for page table 1 which will direct us to regarding page table 2,
-    // then parse second 10 digits and reach the index for page table 2 which will
-    // direct us to regarding offset array, then we will parse the last 12 digits to
-    // get a cell in the offset then paging will be done for that cell....
-    //--------------------------------------------------------------------
+  while (fscanf(input1, "%x", &a) > 0){
+    LIST *node = malloc(sizeof(struct list));
+    start = a;
+    fscanf(input1, "%x", &a);
+    end = a;
+    node->start = start;
+    node->end = end;
+    node->next = NULL;
+    if (head == NULL){
+      head = node;
+      current = node;
+    }else{
+      current->next = node;
+      current = current->next;
+    }
+  }
 
-    ////// initialization of page tables and offset array.
-    int ***level1 = (int ***) malloc(sizeof(int **)*1024);
-    for (i = 0; i < 1024; i++){
-      int **level2 = (int **) malloc(sizeof(int *)*1024);
-      for (j = 0; j < 1024; j++){
-        int *table = (int *) malloc(sizeof(int)*4096);
-        for (k = 0; k < 4096; k++){
-          table[k] = -1;
+  frames = malloc(frame_size*sizeof(struct frame));
+  for (i = 0; i < frame_size; i++){
+    frames[i].entry_time = 0;
+    frames[i].used_time = 0;
+    frames[i].virtual_address = 0;
+  }
+
+  unsigned int **pagetable = malloc(sizeof(unsigned int*)*1024);
+  for (i = 0; i < 1024; i++){
+    pagetable[i] = malloc(sizeof(unsigned int)*1024);
+  }
+  while (fscanf(input2, "%x", &a) > 0){
+    target = a;
+    current = head;
+    flag = 0;
+    while (current != NULL){
+      if (target >= current->start && target <= current->end){
+        flag = 1;
+      }
+      current = current->next;
+    }
+    if (flag == 0){
+      fputs("e\n", out);
+    }else{
+      ///////////////////////////////////////
+      pg1 = target >> 22;
+      pg2 = target >> 12;
+      pg2 = pg2 & page2perde;
+      offset = target & offset_perde;
+      check = pagetable[pg1][pg2] << 12;
+      check = check | offset;
+      /*printf("pg1: %u\tpg2: %u\toffset: %u\tcheck: %u\n", pg1, pg2, offset, check);
+      printf("target: %u\tframe: %u\n", target, frames[check].virtual_address);*/
+      if (frames[check].virtual_address == target){
+        sprintf(buffer, "%08x", target);
+        fputs("0x", out);
+        fputs(buffer, out);
+        fputs("\n", out);
+        used_time++;
+        frames[check].used_time = used_time;
+      }else{
+        ////// page fault case
+        empty = 0;
+        for (i = offset; i < frame_size; i=i+4096){
+          if (frames[i].entry_time == 0){
+            empty = 1;
+            break;
+          }
         }
-        level2[j] = table;
+        if (empty == 1){
+          frames[i].virtual_address = target;
+          entry_time++;
+          used_time++;
+          frames[i].entry_time = entry_time;
+          frames[i].used_time = used_time;
+          pagetable[pg1][pg2] = i / 4096;
+          sprintf(buffer, "%08x", target);
+          fputs("0x", out);
+          fputs(buffer, out);
+          fputs(" x\n", out);
+        }else{
+          // LRU
+          if (algo == 1){
+            victim = offset;
+            for (j = offset; j < frame_size; j=j+4096){
+              if (frames[j].used_time < frames[victim].used_time){
+                victim = j;
+              }
+            }
+          }else{
+            // FIFO
+            victim = offset;
+            for (j = offset; j < frame_size; j=j+4096){
+              if (frames[j].entry_time < frames[victim].entry_time){
+                victim = j;
+              }
+            }
+          }
+          frames[victim].virtual_address = target;
+          entry_time++;
+          used_time++;
+          frames[victim].entry_time = entry_time;
+          frames[victim].used_time = used_time;
+          pagetable[pg1][pg2] = victim / 4096;
+          sprintf(buffer, "%08x", target);
+          fputs("0x", out);
+          fputs(buffer, out);
+          fputs(" x\n", out);
+        }
       }
-      level1[i] = level2;
-    }
-
-    ///// initialization of the physical address array
-    PHY_A **frames = (PHY_A **) malloc(sizeof(PHY_A *)*frame_array_size);
-    for (i = 0; i < frame_array_size; i++){
-      PHY_A *node = (PHY_A *) malloc(sizeof(PHY_A));
-      node->virt_addr = -1;
-      frames[i] = node;
-    }
-
-    ///---------------------------------------
-    /// things to do:
-    ///   *test requests for physical addresses without considering page faults
-    ///   *implement LRU and FIFO for selecting victim page in case of page faults
-    ///   *add -r parameter case
-    ///   *run tests
-    ///---------------------------------------
-
-    fclose(fp1);
-    fclose(fp2);
-  }else{
-    ////// if the first parameter is -r
-  }
-}
-char *hexToBin(char *buffer){
-  char *binary = (char *) malloc(sizeof(char)*32);
-  int i;
-
-  for (i = 2; i < strlen(buffer); i++){
-    printf("buffer[i] is: %d\n", buffer[i]);
-    if (buffer[i] == 48)
-      strcat(binary, "0000");
-    if (buffer[i] == 49)
-      strcat(binary, "0001");
-    if (buffer[i] == 50)
-      strcat(binary, "0010");
-    if (buffer[i] == 51)
-      strcat(binary, "0011");
-    if (buffer[i] == 52)
-      strcat(binary, "0100");
-    if (buffer[i] == 53)
-      strcat(binary, "0101");
-    if (buffer[i] == 54)
-      strcat(binary, "0110");
-    if (buffer[i] == 55)
-      strcat(binary, "0111");
-    if (buffer[i] == 56)
-      strcat(binary, "1000");
-    if (buffer[i] == 57)
-      strcat(binary, "1001");
-    if (buffer[i] == 65)
-      strcat(binary, "1010");
-    if (buffer[i] == 66)
-      strcat(binary, "1011");
-    if (buffer[i] == 67)
-      strcat(binary, "1100");
-    if (buffer[i] == 68)
-      strcat(binary, "1101");
-    if (buffer[i] == 69)
-      strcat(binary, "1110");
-    if (buffer[i] == 70)
-      strcat(binary, "1111");
-    if (buffer[i] == 97)
-      strcat(binary, "1010");
-    if (buffer[i] == 98)
-      strcat(binary, "1011");
-    if (buffer[i] == 99)
-      strcat(binary, "1100");
-    if (buffer[i] == 100)
-      strcat(binary, "1101");
-    if (buffer[i] == 101)
-      strcat(binary, "1110");
-    if (buffer[i] == 102)
-      strcat(binary, "1111");
-  }
-  return binary;
-}
-char *pageLevelOne(char *binary){
-  char *pt1 = (char *) malloc(sizeof(char)*10);
-  int i;
-
-  for (i = 0; i < 10; i++){
-    if (binary[i] == 48){
-      strcat(pt1, "0");
-    }else{
-      strcat(pt1, "1");
     }
   }
-  return pt1;
-}
-char *pageLevelTwo(char *binary){
-  char *pt2 = (char *) malloc(sizeof(char)*10);
-  int i;
 
-  for (i = 10; i < 20; i++){
-    if (binary[i] == 48){
-      strcat(pt2, "0");
-    }else{
-      strcat(pt2, "1");
-    }
-  }
-  return pt2;
-}
-char *pageOffset(char *binary){
-  char *offset = (char *) malloc(sizeof(char)*12);
-  int i;
-
-  for (i = 20; i < strlen(binary); i++){
-    if (binary[i] == 48){
-      strcat(offset, "0");
-    }else{
-      strcat(offset, "1");
-    }
-  }
-  return offset;
-}
-int binToDecimal(char *binary){
-  int i,j,result = 0, temp = 1, length = strlen(binary);
-  for (i = 0; i < length; i++){
-    if (binary[i] == 49){
-      for (j = (length - i) - 1; j > 0; j--){
-        temp = temp * 2;
-      }
-      result = result + temp;
-      temp = 1;
-    }
-  }
-  return result;
+  return 0;
 }
